9cc583326298e65ccf58e5d09a809804d948e5c0
Fatih Ozan Arslan <103501454+FOzanArslan@users.noreply.github.com>
2022-06-29 16:06:59 +0300

    Add files via upload

---
+++ ALT-SORGU/ALTSORGU.sql
@@ -0,0 +1,22 @@
+Select COUNT(*) FROM film
+WHERE length >(
+SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) FROM film 
+WHERE rental_rate = (
+SELECT MAX(rental_rate) FROM film);
+
+SELECT * FROM film 
+WHERE rental_rate = (
+	SELECT MIN(rental_rate) FROM film) 
+AND
+replacement_cost = (
+	SELECT MIN(replacement_cost) FROM film);
+	
+SELECT customer.first_name, customer.last_name FROM customer
+JOIN payment ON customer.customer_id = ANY
+(SELECT customer_id FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(customer_id) DESC
+LIMIT 5)
+LIMIT 5;
